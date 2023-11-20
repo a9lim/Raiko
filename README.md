15e7cc0b48a41e554e6dc26dea54ebfa4d860728
southernscreamer32 <aidanlim192@gmail.com>
2023-11-20 02:40:47 -0800

    Updated README

--- README.md
+++ README.md
@@ -3,11 +3,26 @@
 [![Release](https://img.shields.io/github/release/a9lim/raiko.svg)](https://github.com/a9lim/raiko/releases/latest)
 [![License](https://img.shields.io/github/license/a9lim/raiko.svg)](https://github.com/a9lim/raiko/blob/master/LICENSE)
 
-Discord music bot forked from [jagrosh's JMusicBot](https://github.com/jagrosh/MusicBot)
+Discord music + chatbot forked from [jagrosh's JMusicBot](https://github.com/jagrosh/MusicBot)
+
+## Features
+* (Relatively) easy to use 
+* Supports all sources supported by [LavaPlayer](https://github.com/lavalink-devs/lavaplayer)
+* OpenAI chatbot integrated with configurable prompt
+* Improved performance
+
+## Setup
+1. [Install Java 21](https://www.oracle.com/java/technologies/downloads/#java21)
+2. Download the .jar file in the [latest release](https://github.com/a9lim/Raiko/releases)
+3. Get a [Discord bot token](https://github.com/jagrosh/MusicBot/wiki/Getting-a-Bot-Token),
+your [Discord user ID](https://github.com/jagrosh/MusicBot/wiki/Finding-Your-User-ID),
+and an [OpenAI API key](https://help.openai.com/en/articles/4936850-where-do-i-find-my-api-key)
+4. Save a copy of [this file](https://github.com/a9lim/Raiko/blob/main/src/main/resources/reference.conf) as 'config.txt' in the same folder as the .jar file
+5. Run the .jar file with `java -jar /path/to/raiko.jar`
 
 ## Main Changes
 * Migrated to Java 21
 * Reworked to use [JDA 5.0.0](https://github.com/discord-jda/JDA), [LavaPlayer 2.0.3](https://github.com/lavalink-devs/lavaplayer), and several other updated libraries
 * [JDA-Utilities](https://github.com/JDA-Applications/JDA-Utilities) forked and directly incorporated into project
 * ChatGPT-powered chatbot integrated 
-* General improvements to code efficiency 
+* General improvements to code efficiency
---
+++ src/test/java/a9lim/raiko/Test.java
@@ -0,0 +1,102 @@
+// Copyright 2023 Aidan Lim (a9lim) <aidanlim192@gmail.com>
+// Copyright 2016-2018 John Grosh (jagrosh) <john.a.grosh@gmail.com> & Kaidan Gustave (TheMonitorLizard).
+//
+// This file is part of Raiko.
+//
+// Raiko is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Raiko is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Raiko. If not, see <http://www.gnu.org/licenses/>.
+
+package a9lim.raiko;
+
+import a9lim.raiko.queue.DoubleDealingQueue;
+
+import java.util.Iterator;
+
+public class Test {
+
+    public static void main(String[] args){
+        testMoveTo(10);
+
+    }
+    public static void testShuffleMine(int len){
+        DoubleDealingQueue<TestQueuable<Integer>> queue = new DoubleDealingQueue<>();
+        int[] arr = new int[len];
+        for(int i = 0; i < len; i++)
+            arr[i] = i+10;
+
+        for(int s: arr)
+            queue.add(new TestQueuable<>(s,(int)(Math.random()+0.25)));
+
+        queue.getDeque().forEach(t -> System.out.print(t + "  "));
+        System.out.println();
+        queue.getDeque().forEach(t -> System.out.print("0" + t.getIdentifier() + "  "));
+
+        System.out.println();
+
+        System.out.println();
+        queue.shuffle(1);
+
+        queue.getDeque().forEach(t -> System.out.print(t + "  "));
+        System.out.println();
+        queue.getDeque().forEach(t -> System.out.print("0" + t.getIdentifier() + "  "));
+
+        System.out.println();
+
+    }
+    public static void testShuffle(int num, int len){
+        DoubleDealingQueue<TestQueuable<Integer>> queue = new DoubleDealingQueue<>();
+        int[] arr = new int[len];
+        for(int i = 0; i < len; i++)
+            arr[i] = i;
+
+        for(int s: arr)
+            queue.add(new TestQueuable<>(s));
+
+        queue.getDeque().forEach(t -> System.out.print(t + "  "));
+
+        System.out.println();
+        queue.shuffle();
+
+        queue.getDeque().forEach(t -> System.out.print(t + "  "));
+
+        System.out.println();
+        int[] arr2 = new int[queue.size()];
+
+        for(int i = 0; i < num; i++) {
+            queue.shuffle();
+            Iterator<TestQueuable<Integer>> iterator = queue.getDeque().iterator();
+            for (int j = 0; j < arr2.length; j++) {
+                arr2[j] += iterator.next().getS();
+            }
+        }
+        for(int i : arr2)
+            System.out.print(i/(double)num + " ");
+    }
+
+    public static void testMoveTo(int len){
+        DoubleDealingQueue<TestQueuable<Integer>> queue = new DoubleDealingQueue<>();
+        int[] arr = new int[len];
+        for(int i = 0; i < len; i++)
+            arr[i] = i+10;
+
+        for(int s: arr)
+            queue.add(new TestQueuable<>(s));
+
+        queue.getDeque().forEach(t -> System.out.print(t + "  "));
+
+        System.out.println();
+        queue.moveItem(4,7);
+
+        queue.getDeque().forEach(t -> System.out.print(t + "  "));
+    }
+}
